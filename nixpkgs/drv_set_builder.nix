a7b070318076a19afc54f3c4294beba1fce84423
regnat <rg@regnat.ovh>
2018-12-06 17:26:56 +0100

    Remove the `symlinkJoin` hack
    `nix-store --realize /…foo.drv!out` will only request the `out` output of
    `foo.drv` which is what we needed the wrapping for

--- nixpkgs/drv_set_builder.nix
+++ nixpkgs/drv_set_builder.nix
@@ -3,19 +3,9 @@
 }:
 with nixpkgs;
 let
-  # `nix-store --realize` will build *all* the outputs of the provided drv file.
-  # Since we only want to access to the specified output, we wrap it into a
-  # single-output derivation pointing to it.
-  wrapOutput = target:
-    if !(target ? name) then null else
-    symlinkJoin {
-      name = target.name;
-      paths = [ target ];
-    };
-
   # Returns the path to a `.drv` file corresponding to the provided derivation
   evaluateElement = x:
-    let result = builtins.tryEval ((wrapOutput x).drvPath or null); in
+    let result = builtins.tryEval (x.drvPath + "!" + x.outputName); in
     if result.success == true then result.value else null;
 in
 (builtins.mapAttrs (_: evaluateElement) (packages nixpkgs))
