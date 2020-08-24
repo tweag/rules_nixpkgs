91afd11682b46825fa7bbf7d71f7fca6dcd5633b
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-08-24 14:45:43 +0200

    Generate the README file from a template
    This substitutes the Stardoc generated API documentation into the README
    template and defines automation to update the project README file and
    test that it is up-to-date on CI.

---
+++ docs/update-readme.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+set -euo pipefail
+
+new="$(rlocation io_tweag_rules_nixpkgs/docs/README.md)"
+
+# this variable is set by `bazel run`
+if [ -z "$BUILD_WORKSPACE_DIRECTORY" ]; then
+    echo "This script must be executed using bazel run." >&2
+    exit 1
+fi
+
+cp "$new" "$BUILD_WORKSPACE_DIRECTORY/README.md"
