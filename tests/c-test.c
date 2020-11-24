85ee736a938bc882ee8cb2eb312c9b4c66f53a6c
Andreas Herrmann <andreas.herrmann@tweag.io>
2020-11-24 10:34:55 +0100

    Test C and C++ compilation explicitly

---
+++ tests/c-test.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+int main() {
+        puts("Hello world\n");
+        // Ensure that this is compiled as C, template is a keyword in C++.
+        int template = 0;
+        return template;
+}
