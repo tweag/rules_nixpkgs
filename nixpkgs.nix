472383e5341592fd2bfcae0db69673b245edc101
Andreas Herrmann <andreas.herrmann@tweag.io>
2021-02-08 15:04:25 +0100

    Update Bazel 3.3.1 --> 3.7.2

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,5 @@
 let
-  # nixpkgs-unstable as of 2020-07-09
+  # nixpkgs-unstable as of 2021-02-04
   spec = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
   nixpkgs = fetchTarball {
     url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
