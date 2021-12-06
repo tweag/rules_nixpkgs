e7cf47135ab3b635028109a4f16e8d64c087a731
Alexei Drake <alexei.drake@tweag.io>
2021-12-06 17:05:07 +0100

    Pin nixpkgs in example shells

--- examples/toolchains/go/shell.nix
+++ examples/toolchains/go/shell.nix
@@ -1,2 +1,6 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (builtins.fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz";
+  sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+}) { } }:
+
 pkgs.mkShell { nativeBuildInputs = [ pkgs.bazel_4 ]; }
