bc62449178697ee706c6f9a7d7c290102b639267
Andreas Herrmann <andreas.herrmann@tweag.io>
2022-02-24 14:00:12 +0100

    Fix shopt: inherit_errexit: invalid shell option name

--- examples/toolchains/java/shell.nix
+++ examples/toolchains/java/shell.nix
@@ -1,6 +1,3 @@
-{ pkgs ? import (builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/7f9b6e2babf232412682c09e57ed666d8f84ac2d.tar.gz";
-  sha256 = "03nb8sbzgc3c0qdr1jbsn852zi3qp74z4qcy7vrabvvly8rbixp2";
-}) { } }:
+{ pkgs ? import ./nixpkgs.nix { } }:
 
 pkgs.mkShell { nativeBuildInputs = [ pkgs.bazel_5 ]; }
