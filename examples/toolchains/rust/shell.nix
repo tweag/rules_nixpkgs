b2e21651a7738ccb06367514b1289c9ef057c9ed
Ben Radford <benradf@users.noreply.github.com>
2023-01-10 16:14:50 +0000

    Add security framework library for MacOS.

--- examples/toolchains/rust/shell.nix
+++ examples/toolchains/rust/shell.nix
@@ -1,3 +1,9 @@
 { pkgs ? import ./nixpkgs.nix { } }:
 with pkgs;
-mkShell { nativeBuildInputs = [ bazel_5 git nix zlib libiconv ]; }
+mkShell {
+  nativeBuildInputs = [ bazel_5 git nix zlib libiconv ]
+    ++ (if stdenv.isDarwin then
+      [ darwin.apple_sdk.frameworks.Security ]
+    else
+      [ ]);
+}
