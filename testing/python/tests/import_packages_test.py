4f23a38594d3c136a224bd80838e18da1d7b6a0e
Guillaume Maudoux <guillaume.maudoux@tweag.io>
2023-03-12 21:59:04 +0100

    Add a test for python dependencies

--- testing/python/tests/import_packages_test.py
+++ testing/python/tests/import_packages_test.py
@@ -3,3 +3,8 @@ print("cffi version = ", cffi.__version__)
 
 import pandas
 print("pandas version = ", pandas.__version__)
+
+# Also test that numpy is available.
+# nixpkgs_python_repository should provide the dependencies closure.
+import numpy
+print("numpy version = ", numpy.__version__)
