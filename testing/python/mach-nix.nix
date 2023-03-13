f2af8430c23241c0b7b282caa3ed66e5aded5788
Guillaume Maudoux <guillaume.maudoux@tweag.io>
2023-03-13 17:02:22 +0100

    Another attempt at mach-nix

--- testing/python/mach-nix.nix
+++ testing/python/mach-nix.nix
@@ -9,11 +9,13 @@ let
     # ** Extremely important! **
     # You want to use the same python version as your main nixpkgs.
     # Bazel does not enforce this, and it will fail for binary packages.
-    pkgs = nixpkgs;
+    #pkgs = nixpkgs; # broken with this version of mach-nix
+    python = "python310";
+    pypiDataRev = "cc1357c73b483fdec3091ea782aafc86a6a79fe1";
+    pypiDataSha256 = "042gn8m6iw7kcwn5qzdqxvhw5q1k6y3h1jww0r746mfmi0qn5i80";
   };
   pythonWithPackages = mach-nix.mkPython {
     requirements = builtins.readFile ./requirements.txt;
-    providers._default = "nixpkgs";
   };
   python = pythonWithPackages.python;
   # Beloved hacks !
