d70da9691e1308bacf7deb0cf540bb0aff766019
Andreas Herrmann <andreas.herrmann@tweag.io>
2023-09-27 14:54:44 +0200

    Update to NixOS-unstable and Bazel 6.3.2

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
