395e5307a0c5e16bca757f3ab67341374fb4cb9c
Claudio Bley <claudio.bley@gmail.com>
2024-07-08 12:46:36 +0200

    Add cacert to nix shell for rust example

--- examples/toolchains/rust/shell.nix
+++ examples/toolchains/rust/shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import ./nixpkgs.nix { } }:
 with pkgs;
 mkShell {
-  nativeBuildInputs = [ bazel_6 git nix zlib libiconv ]
+  nativeBuildInputs = [ bazel_6 cacert git nix zlib libiconv ]
     ++ (lib.optional stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ]);
 }
