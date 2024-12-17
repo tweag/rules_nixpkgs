aeb209a8863ad0646ef2b76d537d811079959dc3
Daniel KT <daniel.thagard@tweag.io>
2024-12-17 15:14:17 -0500

    Upgrade to nixpkgs 24.11

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -11,21 +11,34 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        devShells.default = with pkgs; mkShell {
-          # do not use Xcode on macOS
-          BAZEL_USE_CPP_ONLY_TOOLCHAIN = "1";
-          # for nixpkgs cc wrappers, select C++ explicitly (see https://github.com/NixOS/nixpkgs/issues/150655)
-          BAZEL_CXXOPTS = "-x:c++";
+        devShells.default =
+          with pkgs;
+          mkShell {
+            # do not use Xcode on macOS
+            BAZEL_USE_CPP_ONLY_TOOLCHAIN = "1";
+            # for nixpkgs cc wrappers, select C++ explicitly (see https://github.com/NixOS/nixpkgs/issues/150655)
+            BAZEL_CXXOPTS = "-x:c++";
 
-          name = "rules_nixpkgs_shell";
-          buildInputs = lib.optional pkgs.stdenv.isDarwin darwin.cctools;
-          packages = [ bazel_6 bazel-buildtools cacert gcc nix git openssh ];
-        };
-      });
+            name = "rules_nixpkgs_shell";
+            buildInputs = lib.optional pkgs.stdenv.isDarwin darwin.cctools;
+            packages = [
+              bazel_6
+              bazel-buildtools
+              cacert
+              gcc
+              nix
+              git
+              openssh
+            ];
+          };
+      }
+    );
 }
