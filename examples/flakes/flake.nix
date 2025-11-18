733c121cd87e9a3a9f0a4687cf7cdc9d49236028
z8v <86471569+z8v@users.noreply.github.com>
2025-11-18 13:21:18 +0200

    Free up disk space on the ubuntu runners

--- examples/flakes/flake.nix
+++ examples/flakes/flake.nix
@@ -2,7 +2,7 @@
   description = "C++ environment using Nix flakes";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
